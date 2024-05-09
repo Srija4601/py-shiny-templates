ba3aa79007f929e4159adadc80439227ee401cde
Srija4601 <164297235+Srija4601@users.noreply.github.com>
2024-05-09 15:18:35 +0530

    Update app-core.py

--- basic-navigation/app-core.py
+++ basic-navigation/app-core.py
@@ -14,10 +14,20 @@ page1 = ui.navset_card_underline(
     title="Penguins data",
 )
 
+page2 = ui.navset_card_underline(
+    ui.nav_panel("Plot", ui.output_plot("hist")),
+    ui.nav_panel("Table", ui.output_data_frame("data")),
+    footer=ui.input_select(
+        "var", "Select variable", choices=["bill_length_mm", "body_mass_g"]
+    ),
+    title="Penguins data",
+)
+
+
 app_ui = ui.page_navbar(
     ui.nav_spacer(),  # Push the navbar items to the right
     ui.nav_panel("Page 1", page1),
-    ui.nav_panel("Page 2", "This is the second 'page'."),
+    ui.nav_panel("Page 2", page2),
     title="Shiny navigation components",
 )
 
